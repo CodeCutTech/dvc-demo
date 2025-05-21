dc11a740951775afe70c26e9b638f37e4cc1cb61
khuyentran1401 <khuyentran1476@gmail.com>
2025-05-20 19:15:33 -0500

    Version 2 of data

--- src/create_data.py
+++ src/create_data.py
@@ -1,6 +1,6 @@
 
 import pandas as pd
 
-# Create version 1 of dataset
-df_v1 = pd.DataFrame({"feature": [1, 2, 3], "target": [0, 1, 0]})
-df_v1.to_csv("data/test/data.csv", index=False)
+# Create version 2 of dataset
+df_v2 = pd.DataFrame({"feature": [10, 20, 30], "target": [1, 0, 1]})
+df_v2.to_csv("data/test/data.csv", index=False)
